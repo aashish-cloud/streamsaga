cb8ec3d4c398af718f23ead793626c2de7b3a0d3
Aashish <aashish@Aashishs-MacBook-Air.local>
2024-01-18 03:15:18 +0530

    removed block func in actions

--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -34,26 +34,6 @@ export const Actions = ({ userId, isFollowing }: ActionProps) => {
     });
   };
 
-  const handleBlock = () => {
-    startTransition(() => {
-      onBlock(userId)
-        .then((data) =>
-          toast.success(`Blocked the user ${data.blocked.username}`)
-        )
-        .catch(() => toast.error("Something went wrong!"));
-    });
-  };
-
-  const handleUnblock = () => {
-    startTransition(() => {
-      onUnblock(userId)
-        .then((data) =>
-          toast.success(`Unblocked the user ${data.blocked.username}`)
-        )
-        .catch(() => toast.error("Something went wrong!"));
-    });
-  };
-
   const onClick = () => {
     if (isFollowing) {
       handleUnfollow();
@@ -66,12 +46,6 @@ export const Actions = ({ userId, isFollowing }: ActionProps) => {
     <>
       <Button disabled={isPending} onClick={onClick} variant="primary">
         {isFollowing ? "Unfollow" : "Follow"}
-      </Button>
-      <Button disabled={isPending} onClick={handleBlock}>
-        Block
-      </Button>
-      <Button disabled={isPending} onClick={handleUnblock}>
-        Unblock
       </Button>
     </>
   );
